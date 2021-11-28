0df6e673110ebaf37cfce7c5e6893c3ee552162a
Nazariy Kutsalaba <n.kutsalaba.km04@kpi.ua>
2021-11-28 17:52:42 +0200

    Fixing bugs

--- src/Main.java
+++ src/Main.java
@@ -14,36 +14,41 @@ public class Main extends JComponent  {
         Main main = new Main();
         //creating a window object
         JFrame f = new JFrame("DatasetVisualization");
-        f.setSize(540, 960);
+        f.setSize(960, 540);
         f.setVisible(true);
-        f.setLocation(400, 20);
+        f.setLocation(40, 40);
         f.add(main);
     }
 
     @Override
     public void paint(Graphics gr) {
-        BufferedImage bImg = new BufferedImage(540, 960, BufferedImage.TYPE_INT_RGB);
+        BufferedImage bImg = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphy = (Graphics2D)gr;
         Graphics2D grapImage = bImg.createGraphics();
+        //change the origin
+        graphy.translate(0, getHeight() - 1);
+        graphy.scale(1, -1);
+        grapImage.translate(0, getHeight() - 1);
+        grapImage.scale(1, -1);
         //set background color
         grapImage.setColor(Color.WHITE);
         //creating a background
-        grapImage.fillRect(0, 0, 540, 960);
+        grapImage.fillRect(0, 0, 960, 540);
         //set the color of the points
         grapImage.setColor(Color.BLACK);
         String line = null;
         String [] string;
         try {
             //create creating a point object
-            Rectangle point = new Rectangle(0, 0, 1, 1);
+            Rectangle point = new Rectangle(1, -1, 1, 1);
             BufferedReader reader = new BufferedReader(new FileReader("DS6.txt"));
+
             //loop, to read points from the dataset and display them
             while ((line = reader.readLine()) != null) {
                 string = line.split(" ");
-                point.x = Integer.parseInt(string[0]);
-                point.y = Integer.parseInt(string[1]) - 70;
+                point.x = Integer.parseInt(string[1]);
+                point.y = Integer.parseInt(string[0]);
                 graphy.fill(point);
-                point.y = Integer.parseInt(string[1]);
                 grapImage.draw(point);
             }
         } catch (IOException e) {
@@ -57,3 +62,7 @@ public class Main extends JComponent  {
         }
     }
 }
+
+
+
+
